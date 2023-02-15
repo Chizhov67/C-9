949a715820c41af8cd85e9996963ce842e49ee16
Andrey <chizhov67@gmail.com>
2023-02-15 20:05:08 +0300

    Task64

---
+++ Task 64/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleAppTask64
+{
+	internal class Program
+	{
+		public static void printNumber(int N)
+		{
+			if (N == 0)
+			{
+				return;
+			}
+
+			else
+			{
+				Console.Write(N + " ");
+				printNumber(N - 1);
+			}
+		}
+		static void Main(string[] args)
+		{
+			int N;
+
+			Console.Write("Введите натуральное число N (больше 1): ");
+
+			while ((!int.TryParse(Console.ReadLine(), out N) || N < 1))
+			{
+				Console.Write("Неверный ввод, Введите натуральное число N (больше 1): ");
+			}
+
+			printNumber(N);
+			Console.ReadLine();
+		}
+	}
+}
+
