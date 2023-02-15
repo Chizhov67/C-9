b217c458e022624cd3768e97a664b60c4f915f2e
Andrey <chizhov67@gmail.com>
2023-02-15 20:16:12 +0300

    Task68

---
+++ Task 68/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ConsoleAppTask68
+{
+	internal class Program
+	{
+		static int AkkermanFunction(int N, int M)
+		{
+			if (N == 0)
+			{
+				return M + 1;
+			}
+
+			else if (N != 0 && M == 0)
+			{
+				return AkkermanFunction(N - 1, 1);
+			}
+
+			else
+			{
+				return AkkermanFunction(N - 1, AkkermanFunction(N, M - 1));
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			int N, M;
+
+			Console.Write("Введите положительно число N: ");
+
+			while ((!int.TryParse(Console.ReadLine(), out N) || N < 0))
+			{
+				Console.Write("Неверный ввод, Введите положительно число N: ");
+			}
+
+			Console.Write("Введите положительно число M: ");
+
+			while ((!int.TryParse(Console.ReadLine(), out M) || M < 0))
+			{
+				Console.Write("Неверный ввод, Введите положительно число M: ");
+			}
+
+			Console.WriteLine("\nЗначение функции Аккермана: " + AkkermanFunction(N, M));
+
+			Console.ReadLine();
+		}
+	}
+}
+
+
